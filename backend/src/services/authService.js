7e75ebf42551fb444d132167e3a3e89ae8724062
Leonardo Monteiro <leonardoms17@hotmail.com>
2026-05-19 09:39:07 -0300

    instalando JWT e bcrypt para gerar tolkien e criptografar senha e implementando do login com JWT no authService

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -1,9 +1,35 @@
-export async function autenticar(perfil, senha) {
-  // TODO: buscar o usuário no banco pelo perfil
-  // TODO: comparar a senha informada com o hash armazenado
-  // TODO: gerar e retornar um token JWT
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import supabase from '../config/database.js'
+
+export async function autenticar(email, senha) {
+  const {data: usuario, error} = await supabase.from('usuarios')
+  .select('*')
+  .eq('email', email)
+  .single()
+
+  if(error || !usuario) throw new Error("Email ou snha incorretos")
+
+    const senhaCorreta = await bcrypt.compare(senha, usuario.senha_hash)
+    if(!senhaCorreta) throw new Error("Email ou senha incorretos")
+
+
+      const token = jwt.sign(
+        {userId : usuario.id, perfil : usuario.perfil},
+        process.env.JWT_SECRET,
+        {expiresIn: '1d'}
+      )
+      return {
+        token,
+        usuario: {
+          id: usuario.id,
+          nome: usuario.nome,
+          email: usuario.email,
+          perfil: usuario.perfil
+        }
+      }
 }
 
 export async function encerrarSessao(token) {
-  // TODO: invalidar o token no banco de dados
+  return { mensagem: 'Sessão encerrada'}
 }
