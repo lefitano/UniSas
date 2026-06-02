716b58dd3a3b034effc49dced53b99ffec8f7d3a
Leonardo Monteiro <leonardoms17@hotmail.com>
2026-06-02 19:40:23 -0300

    removendo arquivos que foram commitados errados por colaborador

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,12 +1,10 @@
 import { Router } from 'express'
-import perfilRoutes   from './perfilRoutes.js'
 import usuarioRoutes  from './usuarioRoutes.js'
 import authRoutes     from './authRoutes.js'
 import turmaRoutes    from './turmaRoutes.js'
 
 const router = Router()
 
-router.use('/perfis',   perfilRoutes)
 router.use('/usuarios', usuarioRoutes)
 router.use('/auth',     authRoutes)
 router.use('/turmas',   turmaRoutes)
--- backend/src/routes/perfilRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { listar } from '../controllers/perfilController.js'
-
-const router = Router()
-
-router.get('/', listar)
-
-export default router
